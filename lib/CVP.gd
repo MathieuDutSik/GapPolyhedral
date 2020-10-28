fdfd6eb7a8940b835f8c2b07e867a93fd6896171
Mathieu Dutour Sikiric <Mathieu.Dutour@gmail.com>
2020-10-28 11:07:26 +0100

    Some cleaning.

---
+++ lib/CVP.gd
@@ -0,0 +1,4 @@
+DeclareGlobalFunction("General_CVPVallentinProgram_Rational");
+DeclareGlobalFunction("CVPVallentinProgram");
+DeclareGlobalFunction("DualLLLReducedGramMat");
+DeclareGlobalFunction("ClosestAtDistanceVallentinProgram");
